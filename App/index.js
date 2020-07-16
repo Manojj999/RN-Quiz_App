c5b49370531d2583684fe33967dca8170fe831f0
Jasoliya Manoj <48120921+Manojj999@users.noreply.github.com>
2020-07-16 20:19:28 +0530

    Project Initialized

--- App/index.js
+++ App/index.js
@@ -2,6 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
 export default function App() {
   return (
     <View style={styles.container}>
@@ -11,11 +20,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
